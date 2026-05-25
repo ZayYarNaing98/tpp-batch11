c541cde2db6c07c30c03aa70b5b6ba69dc2a0a59
ZayYarNaing <zayarnaing.pp@gmail.com>
2026-05-25 16:59:13 +0630

    Update bootstrap UI

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-API-Compatible" content="ie=edge">
     <title>Batch Create</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div>
-        <h2>Create New Batch</h2>
+    <div class="container">
+        <h2 class="my-4">Create New Batch</h2>
         @if ($errors->any())
-            <div>
+            <div class="alert alert-danger">
                 <ul>
                     @foreach ($errors->all() as $error)
                         <li style="color: red;">{{ $error }}</li>
@@ -22,16 +24,22 @@
         @endif
         <form action="{{ route('batches.store') }}" method="POST">
             @csrf
-            <label for="name">Batch Name:</label>
-            <input type="text" id="name" name="name" placeholder="Enter Batch Name" value="{{ old('name') }}" />
-            <br><br>
-            <label for="description">Description:</label>
-            <textarea id="description" name="description" placeholder="Enter Description">{{ old('description') }}</textarea>
-            <br><br>
-            <button type="submit">+ Create</button>
-            <a href="{{ route('batches.index') }}">Back</a>
+            <div class="mb-3">
+                <label for="name" class="form-label">Batch Name:</label>
+                <input type="text" class="form-control" name="name" placeholder="Enter Batch Name" />
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Batch Description:</label>
+                <input type="text" class="form-control" name="description" placeholder="Enter Batch Name" />
+            </div>
+            <button type="submit" class="btn btn-primary btn-sm">+ Create</button>
+            <a href="{{ route('batches.index') }}" class="btn btn-secondary btn-sm">Back</a>
         </form>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
