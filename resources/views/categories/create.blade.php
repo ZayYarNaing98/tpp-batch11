03da5b5bb30218fd17989362ab5676d9266dbf17
ZayYarNaing <zayarnaing.pp@gmail.com>
2026-05-25 14:40:36 +0630

    Update Category UI

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -6,31 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Category Create</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div>
-        <h2>Create New Category</h2>
-        {{-- {{ dd($errors) }} --}}
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li style="color: red;">{{ $error }}</li>
-                    @endforeach
-                </ul>
+    <div class="container">
+        <h2 class="my-3">Create New Category</h2>
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('categories.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Category Name:</label>
+                        <input type="text" class="form-control" name="name" placeholder="Enter Category Name">
+                        @error('name')
+                            <div class="invalid-feedback d-block ">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-sm me-2">+ Create</button>
+                    <a href="{{ route('categories.index') }}" class="btn btn-secondary btn-sm">Back</a>
+                </form>
             </div>
-        @endif
-        <form action="{{ route('categories.store') }}" method="POST">
-            @csrf
-            <label for="name">Category Name:</label>
-            <input type="text" id="name" name="name" placeholder="Enter Category Name" />
-            <button type="submit">
-                + Create
-            </button>
-            <a href="{{ route('categories.index') }}">Back</a>
-        </form>
+        </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,32 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <div>
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li style="color: red;">{{ $error }}</li>
-                    @endforeach
-                </ul>
+    <div class="container">
+        <h2 class="my-3">Edit Category</h2>
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('categories.update', [$category->id]) }}" method="POST">
+                    @csrf
+                    <label for="name" class="form-label">Category Name</label>
+                    <input type="text" value="{{ $category->name }}" name="name" class="form-control" />
+                    @error('name')
+                        <div class="invalid-feedback d-block ">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                    <button type="submit" class="btn btn-primary btn-sm mt-3">
+                        Update
+                    </button>
+                    <a href="{{ route('categories.index') }}" class="btn btn-secondary btn-sm mt-3">Back</a>
+                </form>
             </div>
-        @endif
-        <form action="{{ route('categories.update', [$category->id]) }}" method="POST">
-            @csrf
-            <label for="name">Category Name:</label>
-            <br>
-            <input type="text" value="{{ $category->name }}" name="name"/>
-            <button type="submit">
-                Update
-            </button>
-            <a href="{{ route('categories.index') }}">Back</a>
-        </form>
+        </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
