dc7848893945b9d3ffa50417f56d12ad4be900ec
ZayYarNaing <zayarnaing.pp@gmail.com>
2026-06-03 16:08:35 +0630

    Add Service

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,14 +6,29 @@ use App\Models\Batch;
 use App\Models\Category;
 use App\Models\Instructor;
 use App\Models\Student;
+use App\Services\BatchService;
+use App\Services\StudentService;
 
 class DashboardController extends Controller
 {
+
+    protected $studentService;
+    protected $batchService;
+    public function __construct(StudentService $studentService, BatchService $batchService)
+    {
+        $this->studentService = $studentService;
+        $this->batchService = $batchService;
+    }
+
     public function index()
     {
+
+        $studentTotal = $this->studentService->studentTotal();
+
+        $batchTotal = $this->batchService->batchTotal();
         return view('dashboard', [
-            'totalStudents'    => Student::count(),
-            'totalBatches'     => Batch::count(),
+            'totalStudents'    => $studentTotal,
+            'totalBatches'     => $batchTotal,
             'totalCategories'  => Category::count(),
             'totalInstructors' => Instructor::count(),
             'recentBatches'    => Batch::latest()->take(5)->get(),
