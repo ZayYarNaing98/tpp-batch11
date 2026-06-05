6e23590fc509f55593abd8bb8cfd6237fca6a1a7
ZayYarNaing <zayarnaing.pp@gmail.com>
2026-06-05 10:57:39 +0630

    Add Cateogry API CRUD

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -33,5 +34,44 @@ class CategoryController extends BaseController
         ]);
 
         return $this->success($category, "Category Created Successfully", 201);
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        if(!$category){
+            return $this->error(null, "Cateogry Not Found", 404);
+        }
+
+        return $this->success($category, "Category Show Successfully", 200);
+    }
+
+    public function update(UpdateCategoryRequest $request, $id)
+    {
+        $category = Category::find($id);
+
+        if(!$category){
+            return $this->error(null, "Cateogry Not Found", 404);
+        }
+
+        $category->update($request->all());
+
+        return $this->success($category, "Category Updated Successfully", 200);
+
+    }
+
+    public function delete($id)
+    {
+        $category = Category::find($id);
+
+        if(!$category)
+        {
+            return $this->error(null, "Cateogry Not Found", 404);
+        }
+
+        $category->delete();
+
+        return $this->success($category, "Category Deleted Successfully", 200);
     }
 }
